0c506d53a943ec28773d7cbd0667d15c4f7a371e
Christian Loch <christian.loch@gmail.com>
2022-02-01 13:36:57 +0100

    data structures for trusted reference and trusted issuer

--- step_impl/base.py
+++ step_impl/base.py
@@ -4,3 +4,7 @@ from time import sleep
 @step("wait for <15> seconds")
 def wait_for_seconds(seconds):
     sleep(int(seconds))
+
+@step("Reference <testcase>")
+def reference(testcase):
+    print(f'Reference to {testcase}')
---
+++ step_impl/gw/data_structures.py
@@ -0,0 +1,40 @@
+from getgauge import step, data_store
+from util import testdata
+from uuid import uuid4
+import json
+
+
+@step("<country> creates a reference")
+def creates_a_reference(country):
+    reference = {
+        'UUID' : str(uuid4()),
+        'URL' : 'https://does.not.exist/123', 
+        'Type' : 'FHIR', 
+        'Version' : '1.3.0',
+        'Country' : testdata.get_country_code(country),
+        'Service' : 'ValueSet', # ValueSet, PlanDefinition, etc.
+        'Thumbprint' : 'abcdefghijklmnopqrstuvqxyz', # TODO: HEX or base64?
+        'SSLPublicKey' : testdata.get_ssl_public_key(country),
+        'Content-Type' : 'application/json',
+        'SignatureType' : 'CMS'
+    }
+
+    data_store.scenario["trusted.reference"] = reference
+    return reference
+
+
+@step("<country> creates a trusted issuer certificate")
+def creates_a_trusted_issuer_certificate(country):
+    trusted_issuer = {
+        'URL' : 'https://does.not.exist/123', 
+        'Type' : 'FHIR', 
+        'Country' : testdata.get_country_code(country),
+        'Thumbprint' : 'abcdefghijklmnopqrstuvqxyz', # TODO: HEX or base64?
+        'Name' : 'ValueSet',
+        'SSLPublicKey' : testdata.get_ssl_public_key(country),
+        'KeyStorageType' : 'JWKS', # or DIDDocument, JKS, etc.
+    }
+
+    data_store.scenario["trusted.issuer"] = trusted_issuer
+    return trusted_issuer
+
